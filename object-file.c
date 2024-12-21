24d9214cad0d2812f981388a48e8dad3fa6afa95
MarvinZeitschner <m.zeitschner@gmail.com>
2024-12-22 00:47:07 +0100

    fix: resource leaks after errors in hash object family of functinos

--- object-file.c
+++ object-file.c
@@ -24,7 +24,7 @@ void get_object_path(char buffer[], const char *hash) {
 }
 
 void decompress_object_file(const char *hash) {
-  char path[PATH_MAX] = {'\0'};
+  char path[PATH_MAX] = {0};
   get_object_path(path, hash);
 
   FILE *file = fopen(path, "rb");
@@ -107,26 +107,27 @@ void decompress_object_file(const char *hash) {
   return;
 }
 
-void compress_file_to_obj_file(const char *path) {
-  char metadata[METADATA_MAX] = {"\0"};
-  char dir_path[PATH_MAX] = {"\0"};
-  char obj_path[PATH_MAX] = {"\0"};
-  hash_object_file(metadata, obj_path, dir_path, path);
-
-  FILE *file = fopen(path, "rb");
-  if (!file) {
-    die("Failed to open file: %s", path);
+int compress_file_to_obj_file(const char *path) {
+  char metadata[METADATA_MAX] = {0};
+  char dir_path[PATH_MAX] = {0};
+  char obj_path[PATH_MAX] = {0};
+  if (hash_object_file(metadata, obj_path, dir_path, path) != 0) {
+    return -1;
   }
 
   create_dir(dir_path);
 
-  // --------------- zlib magic --------------- //
-
-  rewind(file);
+  FILE *file = fopen(path, "rb");
+  if (!file) {
+    perror("");
+    return -1;
+  }
 
   FILE *obj_file = fopen(obj_path, "wb");
   if (!obj_file) {
-    die("Failed to open file %s: %s", obj_path, strerror(errno));
+    fclose(file);
+    perror("");
+    return -1;
   }
 
   int ret, flush;
@@ -139,7 +140,8 @@ void compress_file_to_obj_file(const char *path) {
   if (ret != Z_OK) {
     fclose(file);
     fclose(obj_file);
-    die("Failed to initialize zlib");
+    error("Failed to initialize zlib");
+    return -1;
   }
   /**
    * Append the metadata to the in buffer to get compressed
@@ -151,11 +153,17 @@ void compress_file_to_obj_file(const char *path) {
   strm.avail_out = CHUNK;
   strm.next_out = out;
   ret = deflate(&strm, Z_NO_FLUSH);
-  assert(ret != Z_STREAM_ERROR);
+  if (ret == Z_STREAM_ERROR) {
+    fclose(file);
+    fclose(obj_file);
+    die("A zlib error occurred");
+  }
   have = CHUNK - strm.avail_out;
   if (fwrite(out, 1, have, obj_file) != have || ferror(obj_file)) {
     (void)deflateEnd(&strm);
-    // return Z_ERRNO;
+    fclose(file);
+    fclose(obj_file);
+    return -1;
   }
 
   do {
@@ -165,7 +173,7 @@ void compress_file_to_obj_file(const char *path) {
       (void)deflateEnd(&strm);
       fclose(file);
       fclose(obj_file);
-      // return Z_ERRNO;
+      return -1;
     }
     flush = feof(file) ? Z_FINISH : Z_NO_FLUSH;
     strm.next_in = in;
@@ -178,7 +186,9 @@ void compress_file_to_obj_file(const char *path) {
       have = CHUNK - strm.avail_out;
       if (fwrite(out, 1, have, obj_file) != have || ferror(obj_file)) {
         (void)deflateEnd(&strm);
-        // return Z_ERRNO;
+        fclose(file);
+        fclose(obj_file);
+        return -1;
       }
     } while (strm.avail_out == 0);
   } while (flush != Z_FINISH);
@@ -188,7 +198,7 @@ void compress_file_to_obj_file(const char *path) {
   fclose(file);
   fclose(obj_file);
 
-  return;
+  return 0;
 }
 
 // TODO: add buffer param to return hash, bc of -w flag
@@ -254,7 +264,7 @@ int hash_object_file(char *metadata, char *obj_path, char *dir_path,
   printf("dir_path: %s\n", dir_path);
   printf("obj_path: %s\n", obj_path);
 
-  return 1;
+  return 0;
 }
 
 int compress_buf(const char *buf, size_t size, const char *path_to_write) {
--- object-file.h
+++ object-file.h
@@ -10,7 +10,7 @@ void get_object_path(char buffer[], const char *hash);
 
 void decompress_object_file(const char *hash);
 
-void compress_file_to_obj_file(const char *path);
+int compress_file_to_obj_file(const char *path);
 
 int hash_object_file(char *metadata, char *dir_path, char *obj_path,
                      const char *path);
